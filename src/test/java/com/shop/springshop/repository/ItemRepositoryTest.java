b43bb99cc6b361ab13c763ff31beb807d5ea67b7
sunhak20 <sunhak20@naver.com>
2022-08-13 18:50:25 +0900

    junit test

---
+++ src/test/java/com/shop/springshop/repository/ItemRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.shop.springshop.repository;
+
+import com.shop.springshop.constant.ItemSellSatus;
+import com.shop.springshop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 리스트")
+    public void creteItemTest(){
+        Item item = new Item();
+        item.setItemName("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellSatus(ItemSellSatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println("savedItem = " + savedItem);
+    }
+}
