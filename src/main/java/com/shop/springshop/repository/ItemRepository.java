94af3ec4ee56e29398949784fb2c1c145db3d7a3
sunhak20 <sunhak20@naver.com>
2022-08-16 21:19:25 +0900

    QuerydslPredicateExecutor read test

--- src/main/java/com/shop/springshop/repository/ItemRepository.java
+++ src/main/java/com/shop/springshop/repository/ItemRepository.java
@@ -3,11 +3,12 @@ package com.shop.springshop.repository;
 import com.shop.springshop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
     List<Item> findByItemNm(String itemNm);
 
--- src/test/java/com/shop/springshop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/springshop/repository/ItemRepositoryTest.java
@@ -1,20 +1,26 @@
 package com.shop.springshop.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.springshop.constant.ItemSellSatus;
 import com.shop.springshop.entity.Item;
 import com.shop.springshop.entity.QItem;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
+
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -130,6 +136,61 @@ class ItemRepositoryTest {
         
         for(Item item : itemList){
             System.out.println("item.toString() = " + item.toString());
+        }
+    }
+
+    public void createItemList2(){
+        for (int i = 0; i < 6; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" +  i);
+            item.setPrice(10000+i);
+            item.setItemDetail("테스트 상품 상세 내용" + i);
+            item.setItemSellSatus(ItemSellSatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+
+        for (int i = 6; i < 11; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" +  i);
+            item.setPrice(10000+i);
+            item.setItemDetail("테스트 상품 상세 내용" + i);
+            item.setItemSellSatus(ItemSellSatus.SOLD_OUT);
+            item.setStockNumber(0);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품 Querydsl 조회 테스트 2")
+    public void queryDslTest2(){
+        this.createItemList2();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+
+        String itemDetail = "테스트 상품";
+        int price = 10003;
+        String itemSellStat = "SELL";
+
+        booleanBuilder.and(item.itemDetail.like("%"+itemDetail+"%"));
+        booleanBuilder.and(item.price.gt(price));
+
+        if(StringUtils.equals(itemSellStat, ItemSellSatus.SELL.toString())){
+            booleanBuilder.and(item.itemSellSatus.eq(ItemSellSatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0,5);
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+
+        System.out.println("itemPagingResult.getTotalElements() = " + itemPagingResult.getTotalElements());
+    
+        List<Item> resultItemList = itemPagingResult.getContent();
+        for(Item resultItem : resultItemList){
+            System.out.println("resultItem.toString() = " + resultItem.toString());
         }
     }
 }
