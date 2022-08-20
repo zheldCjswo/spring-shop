c41ad1811c2391e5d36ad2b795dab13f09b50d86
sunhak20 <sunhak20@naver.com>
2022-08-20 17:12:19 +0900

    thymeleaf test 01

--- src/main/java/com/shop/springshop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/springshop/controller/ThymeleafExController.java
@@ -1,11 +1,14 @@
 package com.shop.springshop.controller;
 
 
+import com.shop.springshop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -15,5 +18,17 @@ public class ThymeleafExController {
     public String thymeleafExample01(Model model){
         model.addAttribute("data", "타임리프 예제입니다.");
         return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model){
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
     }
 }
