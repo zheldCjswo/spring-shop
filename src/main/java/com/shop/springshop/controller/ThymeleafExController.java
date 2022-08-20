e8765d6ac9ed9bbc52569e171b137d83230ef506
sunhak20 <sunhak20@naver.com>
2022-08-20 18:39:18 +0900

    thymeleaf test 03

--- src/main/java/com/shop/springshop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/springshop/controller/ThymeleafExController.java
@@ -54,4 +54,23 @@ public class ThymeleafExController {
         return "thymeleafEx/thymeleafEx03";
     }
 
+    @GetMapping(value = "/ex04")
+    public String thymeleafExample04(Model model){
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i = 0; i <= 10; i++) {
+
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(10000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
 }
