b3e7580310af22991a3a72c70768d000d1bca126
sunhak20 <sunhak20@naver.com>
2022-08-20 19:43:52 +0900

    thymeleaf test 05

--- src/main/java/com/shop/springshop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/springshop/controller/ThymeleafExController.java
@@ -78,4 +78,11 @@ public class ThymeleafExController {
     public String thymeleafExample05(){
         return "thymeleafEx/thymeleafEx05";
     }
+
+    @GetMapping("/ex06")
+    public String thymeleafExample06(String param1, String param2, Model model){
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+        return "thymeleafEx/thymeleafEx06";
+    }
 }
