55b096ca6239f04f5227edcb27fd60e4b47d2e5e
sunhak20 <sunhak20@naver.com>
2023-01-12 21:02:07 +0900

    post member add

--- src/main/java/com/shop/springshop/controller/MemberController.java
+++ src/main/java/com/shop/springshop/controller/MemberController.java
@@ -1,12 +1,15 @@
 package com.shop.springshop.controller;
 
 import com.shop.springshop.dto.MemberFormDto;
+import com.shop.springshop.entity.Member;
 import com.shop.springshop.service.MemberService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/members")
@@ -15,10 +18,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String memberForm(MemberFormDto memberFormDto){
+        Member member = memberFormDto.createMember(passwordEncoder);
+        memberService.saveMember(member);
+        return "redirect:/";
     }
 }
