4f81d289e51ac5a12440603763b58ce39b3bce51
sunhak20 <sunhak20@naver.com>
2023-01-12 20:57:38 +0900

    login form add

---
+++ src/main/java/com/shop/springshop/controller/MemberController.java
@@ -0,0 +1,24 @@
+package com.shop.springshop.controller;
+
+import com.shop.springshop.dto.MemberFormDto;
+import com.shop.springshop.service.MemberService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/new")
+    public String memberForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+}
