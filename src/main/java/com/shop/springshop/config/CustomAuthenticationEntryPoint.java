80afa1e1150439589e7dde3dbbd5309f8d014604
sunhak20 <sunhak20@naver.com>
2023-01-13 20:34:55 +0900

    itemController add

--- src/main/java/com/shop/springshop/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/shop/springshop/config/CustomAuthenticationEntryPoint.java
@@ -11,6 +11,11 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+
+        if("XMLHttpRequest".equals(request.getHeader("x-requested-with"))){
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            response.sendRedirect("/members/login");
+        }
     }
 }
