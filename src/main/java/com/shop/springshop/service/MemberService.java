a6e5d144736bb216f1e5794f285320ecd7335d6b
sunhak20 <sunhak20@naver.com>
2023-01-13 19:19:45 +0900

    loadUserByUsername add

--- src/main/java/com/shop/springshop/service/MemberService.java
+++ src/main/java/com/shop/springshop/service/MemberService.java
@@ -3,6 +3,10 @@ package com.shop.springshop.service;
 import com.shop.springshop.entity.Member;
 import com.shop.springshop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,7 +14,7 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService  {
 
     private final MemberRepository memberRepository;
 
@@ -24,5 +28,21 @@ public class MemberService {
         if(findMember != null){
             throw new IllegalArgumentException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
+
     }
 }
