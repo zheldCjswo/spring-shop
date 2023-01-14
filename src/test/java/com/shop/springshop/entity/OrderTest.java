305c169feb4547aa11431db9997866b4101e0215
sunhak20 <sunhak20@naver.com>
2023-01-14 09:33:06 +0900

    lazy option add

--- src/test/java/com/shop/springshop/entity/OrderTest.java
+++ src/test/java/com/shop/springshop/entity/OrderTest.java
@@ -118,6 +118,8 @@ class OrderTest {
         OrderItem orderItem = orderItemRepository.findById(orderItemId)
                 .orElseThrow(EntityNotFoundException::new);
         System.out.println("Order class : " + orderItem.getOrder().getClass());
+
+        System.out.println(orderItem.getOrder().getOrderDate());
     }
 
 }
