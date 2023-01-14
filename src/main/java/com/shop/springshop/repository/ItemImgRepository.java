3fd0edbf3b5dce8f629bc1621e85b99899f004ba
sunhak20 <sunhak20@naver.com>
2023-01-14 17:01:38 +0900

    file upload test

--- src/main/java/com/shop/springshop/repository/ItemImgRepository.java
+++ src/main/java/com/shop/springshop/repository/ItemImgRepository.java
@@ -3,5 +3,10 @@ package com.shop.springshop.repository;
 import com.shop.springshop.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+
 }
---
+++ src/test/java/com/shop/springshop/service/ItemServiceTest.java
@@ -0,0 +1,79 @@
+package com.shop.springshop.service;
+
+import com.shop.springshop.constant.ItemSellSatus;
+import com.shop.springshop.dto.ItemFormDto;
+import com.shop.springshop.entity.Item;
+import com.shop.springshop.entity.ItemImg;
+import com.shop.springshop.repository.ItemImgRepository;
+import com.shop.springshop.repository.ItemRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.EntityExistsException;
+import javax.transaction.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemServiceTest {
+
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    ItemImgRepository itemImgRepository;
+
+    List<MultipartFile> createMultiFiles() throws Exception {
+       List<MultipartFile> multipartFiles = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            String path = "C:/shop/item/";
+            String imageName = "image" + i + ".jpg";
+            MockMultipartFile multipartFile = new MockMultipartFile(path, imageName, "image/jpg", new byte[]{1,2,3,4});
+            multipartFiles.add(multipartFile);
+        }
+
+        return multipartFiles;
+    }
+
+    @Test
+    @DisplayName("상품 등록 테스트")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void saveItem() throws Exception {
+        ItemFormDto itemFormDto = new ItemFormDto();
+        itemFormDto.setItemNm("테스트 상품");
+        itemFormDto.setItemSellSatus(ItemSellSatus.SELL);
+        itemFormDto.setItemDetail("테스트 상품입니다.");
+        itemFormDto.setPrice(1000);
+        itemFormDto.setStockNumber(10);
+
+        List<MultipartFile> multipartFiles = createMultiFiles();
+        Long itemId = itemService.saveItem(itemFormDto, multipartFiles);
+
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(EntityExistsException::new);
+
+        assertEquals(itemFormDto.getItemNm(), item.getItemNm());
+        assertEquals(itemFormDto.getItemSellSatus(), item.getItemSellSatus());
+        assertEquals(itemFormDto.getItemDetail(), item.getItemDetail());
+        assertEquals(itemFormDto.getPrice(), item.getPrice());
+        assertEquals(itemFormDto.getStockNumber(), item.getStockNumber());
+        assertEquals(multipartFiles.get(0).getOriginalFilename(), itemImgList.get(0).getOriImgName());
+    }
+}
