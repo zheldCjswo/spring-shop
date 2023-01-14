c4a6968fec4cac97bb51ce0672934d583f2867bb
sunhak20 <sunhak20@naver.com>
2023-01-14 09:57:19 +0900

    baseEntity

--- src/main/java/com/shop/springshop/entity/BaseEntity.java
+++ src/main/java/com/shop/springshop/entity/BaseEntity.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
--- src/main/java/com/shop/springshop/entity/Member.java
+++ src/main/java/com/shop/springshop/entity/Member.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
