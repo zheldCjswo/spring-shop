7f8dca6aeebd5076e55ede3875bfffcd2eae4872
sunhak20 <sunhak20@naver.com>
2023-01-14 18:59:09 +0900

    item update

--- src/main/java/com/shop/springshop/controller/ItemController.java
+++ src/main/java/com/shop/springshop/controller/ItemController.java
@@ -66,4 +66,26 @@ public class ItemController {
 
         return "item/itemForm";
     }
+
+    @PostMapping(value = "/admin/item/{itemId}")
+    public String itemUpdate(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "item/itemForm";
+        }
+
+        if(itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null){
+            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값입니다.");
+            return "item/itemForm";
+        }
+
+        try {
+            itemService.updateItem(itemFormDto, itemImgFileList);
+        } catch (Exception e){
+            model.addAttribute("errorMessage", "상품수정 중 에러가 발생되었습니다.");
+            return "item/itemForm";
+        }
+
+        return "redirect:/";
+    }
 }
