bf60e45ca3186972b5ff4e5b8d8fbcc642f85096
sunhak20 <sunhak20@naver.com>
2023-01-14 09:56:03 +0900

    BaseEntity

--- src/main/java/com/shop/springshop/entity/BaseEntity.java
+++ src/main/java/com/shop/springshop/entity/BaseEntity.java
@@ -2,24 +2,24 @@ package com.shop.springshop.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 
 @EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
 @Getter @Setter
-public abstract class BaseTimeEntity {
+public abstract class BaseEntity extends BaseTimeEntity {
 
-    @CreatedDate
+    @CreatedBy
     @Column(updatable = false)
-    private LocalDateTime regTime;
+    private String createdBy;
 
-    @LastModifiedDate
-    private LocalDateTime updateTime;
+    @LastModifiedBy
+    private String modifiedBy;
 }
