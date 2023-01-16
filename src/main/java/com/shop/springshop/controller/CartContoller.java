31c85c7a028ff159fc0d081423ffecbc1e0c7b62
sunhak20 <sunhak20@naver.com>
2023-01-16 19:16:51 +0900

    cartController add

---
+++ src/main/java/com/shop/springshop/controller/CartContoller.java
@@ -0,0 +1,49 @@
+package com.shop.springshop.controller;
+
+import com.shop.springshop.dto.CartItemDto;
+import com.shop.springshop.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class CartContoller {
+
+    private final CartService cartService;
+
+    @PostMapping(value = "/cart")
+    public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto,
+                                              BindingResult bindingResult, Principal principal
+                                              )
+    {
+        if(bindingResult.hasErrors()){
+            StringBuilder sb = new StringBuilder();
+            List<FieldError> fieldErrorList = bindingResult.getFieldErrors();
+            for (FieldError fieldError : fieldErrorList){
+                sb.append(fieldError.getDefaultMessage());
+            }
+            return new ResponseEntity<String>(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
+
+        String email = principal.getName();
+        Long cartItemId;
+
+        try {
+            cartItemId = cartService.addCart(cartItemDto, email);
+        } catch (Exception e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+    }
+}
