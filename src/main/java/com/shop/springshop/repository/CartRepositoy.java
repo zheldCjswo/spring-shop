b7e736fa937a6fd9b87cc0e006dfe7e69c8b7b85
sunhak20 <sunhak20@naver.com>
2023-01-16 19:04:33 +0900

    cart logic update

--- src/main/java/com/shop/springshop/repository/CartRepositoy.java
+++ src/main/java/com/shop/springshop/repository/CartRepositoy.java
@@ -4,4 +4,6 @@ import com.shop.springshop.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepositoy extends JpaRepository<Cart, Long> {
+
+    Cart findByMemberId(Long memberId);
 }
